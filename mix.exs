3b068a0cdfc64ffa09bcdc5f6fb58fcd0574ca63
Tom Clarke <tom@u2i.com>
2026-03-21 13:26:57 -0400

    chore: bump to 0.1.12

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Wallabidi.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/u2i/wallabidi"
-  @version "0.1.11"
+  @version "0.1.12"
   @maintainers ["Tom Clarke"]
 
   def project do
